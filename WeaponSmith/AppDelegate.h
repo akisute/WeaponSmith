e409f6334103ff406eb9039f9112d946b0ef9b03
akisute <akisutesama@gmail.com>
2014-05-10 17:46:06 +0900

    Initial commit

---
+++ WeaponSmith/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeaponSmith
+//
+//  Created by 小野 将司 on 2014/05/10.
+//  Copyright (c) 2014年 akisute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeaponSmithTests/WeaponSmithTests.m
@@ -0,0 +1,34 @@
+//
+//  WeaponSmithTests.m
+//  WeaponSmithTests
+//
+//  Created by 小野 将司 on 2014/05/10.
+//  Copyright (c) 2014年 akisute. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WeaponSmithTests : XCTestCase
+
+@end
+
+@implementation WeaponSmithTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
